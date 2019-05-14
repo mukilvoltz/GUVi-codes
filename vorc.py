416d51d4f2b05b4c8807fe5de5d82b72f27c9e8e
mukilvoltz <50615640+mukilvoltz@users.noreply.github.com>
2019-05-14 15:42:28 +0530

    Update vorc.py

--- vorc.py
+++ vorc.py
@@ -1,15 +1,8 @@
 a=str(input())
-
 if a.isalpha():
-
     if a == "a" or a =="i" or a == "e" or a == "o" or a == "u":
-
         print("Vowel")
-
     else:
-
         print("Consonant")
-
 else:
-
     print("Invalid")
