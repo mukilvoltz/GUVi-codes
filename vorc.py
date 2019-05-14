0cc99e1092bda481b1f1920970775784aeb1a215
mukilvoltz <50615640+mukilvoltz@users.noreply.github.com>
2019-05-14 15:41:56 +0530

    Add files via upload

---
+++ vorc.py
@@ -0,0 +1,15 @@
+a=str(input())
+
+if a.isalpha():
+
+    if a == "a" or a =="i" or a == "e" or a == "o" or a == "u":
+
+        print("Vowel")
+
+    else:
+
+        print("Consonant")
+
+else:
+
+    print("Invalid")
