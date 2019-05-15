d49c5b9beb7cfe940c8f81f65412cee590c70539
mukilvoltz <50615640+mukilvoltz@users.noreply.github.com>
2019-05-15 21:28:59 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -2,10 +2,10 @@ a=int(input())
 n=int(a/2)
 c=0
 for i in range (2,n+1):
-    if a % 2 != 0:
+    if a % i == 0:
         c=1
-        print("yes")
+        print("no")
         break
 if c == 0:
-    print("no")
+    print("yes")
     
