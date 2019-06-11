22d2b4675a1c2612bf37704a7618bf530c9d7af1
mukilvoltz <50615640+mukilvoltz@users.noreply.github.com>
2019-06-11 21:19:55 +0530

    Create primeinterval.py

---
+++ primeinterval.py
@@ -0,0 +1,14 @@
+p,q=input().split()
+p=int(p)
+q=int(q)
+def prime(a):
+    n=int(a/2)
+    c=0
+    for i in range (2,n+1):
+        if a % i == 0:
+            c=1
+            break
+    if c == 0:
+        print(a, end =" ")
+for j in range (p,q):
+    prime(j)
