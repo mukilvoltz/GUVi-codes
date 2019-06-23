19ec66f220bab51b19d27e1677ebca6a0ed36cf5
mukilvoltz <50615640+mukilvoltz@users.noreply.github.com>
2019-06-23 09:05:54 +0530

    Update noofscinword.py

--- noofscinword.py
+++ noofscinword.py
@@ -4,12 +4,12 @@ asc=0
 c=0
 for l in letters:
     asc=ord(l)
-    if asc>33 and asc<47:
+    if asc>=33 and asc<=47:
         c=c+1
-    elif asc>58 and asc<64:
+    elif asc>=58 and asc<=64:
         c=c+1
-    elif asc>91 and asc<96:
+    elif asc>=91 and asc<=96:
         c=c+1
-    elif asc>124 and asc<126:
+    elif asc>=124 and asc<=126:
         c=c+1
 print(c)
