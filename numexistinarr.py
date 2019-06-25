43bafa42293e5689d4509926f242d4cb9cec741a
mukilvoltz <50615640+mukilvoltz@users.noreply.github.com>
2019-06-25 13:35:51 +0530

    Create numexistinarr.py

---
+++ numexistinarr.py
@@ -0,0 +1,12 @@
+l,m=input().split()
+l=int(l)
+m=int(m)
+arr=[int(x) for x in input().split()]
+c=0
+for i in arr:
+    if i == m:
+        print("yes")
+        c=1
+        break
+if c == 0
+    print("no")
