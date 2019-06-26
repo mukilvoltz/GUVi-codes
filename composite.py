b93659d96bcac1381737ab3c89c24b01034c266b
mukilvoltz <50615640+mukilvoltz@users.noreply.github.com>
2019-06-26 11:56:13 +0530

    Update composite.py

--- composite.py
+++ composite.py
@@ -1,10 +1,10 @@
-a=int(input())
-n=int(a/2)
-c=0
+p=int(input())
+n=int(p/2)
+q=0
 for i in range (2,n+1):
-    if a % i == 0:
-        c=1
+    if p % i == 0:
+        q=1
         print("yes")
         break
-if c == 0:
+if q == 0:
     print("no")
