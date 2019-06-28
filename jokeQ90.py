94ee14ab50a003795ba90a025c36fea57cb9a289
mukilvoltz <50615640+mukilvoltz@users.noreply.github.com>
2019-06-28 08:59:54 +0530

    Update jokeQ90.py

--- jokeQ90.py
+++ jokeQ90.py
@@ -1,7 +1,7 @@
-a=str(input())
-num=["1","2","3","4","5","6","7","8","9","0"]
-lett=list(a)
-for x in lett:
-    for i in num:
+h=str(input())
+nums=["1","2","3","4","5","6","7","8","9","0"]
+letts=list(h)
+for x in letts:
+    for i in nums:
         if x==i:
             print(x,end="")
