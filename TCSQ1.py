c3c38dd15254499d3537761ac79bd8473063d33a
mukilvoltz <50615640+mukilvoltz@users.noreply.github.com>
2019-06-29 13:39:32 +0530

    Update TCSQ1.py

--- TCSQ1.py
+++ TCSQ1.py
@@ -5,17 +5,18 @@ b=str(b)
 d=list(a)
 comb=[]
 c=0
-for i in permutations(a,len(a)):
-    n=''.join(i)
-    comb.append(n)
-mi=min(comb)
-l=len(comb)
-for i in range(0,l):
-    if mi>b:
-        print(mi)
-        c=1
-        break
-    comb.remove(mi)
+if a.isdigit() and b.isdigit():
+    for i in permutations(a,len(a)):
+        n=''.join(i)
+        comb.append(n)
     mi=min(comb)
-if c == 0:
+    l=len(comb)
+    for i in range(0,l):
+        if mi>b:
+            print(mi)
+            c=1
+            break
+        comb.remove(mi)
+        mi=min(comb)
+elif c == 0:
     print("-1")
